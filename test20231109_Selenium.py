63ae199d6adbfd4bdd0c87167157032feb12333b
= <=>
2023-11-10 12:39:01 -0800

    Works but number loops is hardcoded

--- test20231109_Selenium.py
+++ test20231109_Selenium.py
@@ -8,21 +8,6 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains 
 
-# def parse_egbc_url(url): 
-# 	# get the first url 
-# 	url = url.split(', ')[0] 
-# 	# split it by '/' 
-# 	splitted_url = url.split('/') 
-# 	# loop over the elements to find where 'cloudfront' url begins 
-# 	for idx, part in enumerate(splitted_url): 
-# 		if 'cloudfront' in part: 
-# 			# add the HTTP scheme and concatenate the rest of the URL 
-# 			# then return the processed url 
-# 			return 'https://' + '/'.join(splitted_url[idx:]) 
-# 	# as we don't know if that's the only measurement to take, 
-# 	# return None if the cloudfront couldn't be found 
-# 	return None
-
 # Define the Chrome webdriver options
 options = webdriver.ChromeOptions() 
 # options.add_argument("--headless") # Set the Chrome webdriver to run in headless mode for scalability
@@ -37,7 +22,7 @@ driver = Chrome(options=options)
 driver.implicitly_wait(5)
 
 url = "https://tools.egbc.ca/Registrant-Directory/Firms/SearchResult" 
- 
+
 driver.get(url) 
 time.sleep(1)
 
@@ -48,51 +33,67 @@ ActionChains(driver).move_to_element(acknowledge_checkbox).click(acknowledge_che
 ActionChains(driver).move_to_element(disclaimer_button).click(disclaimer_button).perform()
 time.sleep(1)
 
-def add_values():
-    table = driver.find_elements(By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/div/div[2]/table")
-    num_row = len(driver.find_elements(By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/div/div[2]/table/tbody/tr"))
-    cells = driver.find_elements(By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/div/div[2]/table/tbody/tr/td")
-    num_col = int(len(cells)/num_row)
+# def add_values():
+# table = driver.find_elements(By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/div/div[2]/table")
+# num_row = len(driver.find_elements(By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/div/div[2]/table/tbody/tr"))
+# cells = driver.find_elements(By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/div/div[2]/table/tbody/tr/td")
+# num_col = int(len(cells)/num_row)
 
-    # create output file
-    outputcolumns = ["Unknown", "Permit Number", "Legal Name", "Also doing business as", "Status", "Main Location"]
-    current_datetime = datetime.now().strftime("%Y-%m-%d %H-%M")
-    filename = current_datetime +" - Selenium.csv"
+# create output filename
+current_datetime = datetime.now().strftime("%Y-%m-%d %H-%M")
+filename = current_datetime +" - Selenium.csv"
 
-    with open(filename,'w',newline='') as f:
-        csv_writer = writer(f)
-        for i in range(0, num_row, 6):
-            out_row = []
-            for j in range(1,6):
-                value = cells[i+j].text
-                out_row.append(value)
-                # print(out_row)
-            csv_writer.writerow(out_row)
+# format storage dataframe
+outputcolumns = [ "Permit Number", "Legal Name", "Also doing business as", "Status", "Main Location"]
+big_df =  pd.DataFrame(columns=outputcolumns)
+# df_row = pd.Series([], dtype=str)
 
-time.sleep(5)
+def scrape_page_to_df(df_columns):
+    # df = pd.DataFrame(columns=df_columns)
+    df = pd.DataFrame()
+    # set up the lists
+    unknowns = []
+    permits = []
+    legal_names = []
+    also_knowns = []
+    status = []
+    locations = []
 
-next_page = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/div/div[2]/div/a[3]")
-ActionChains(driver).move_to_element(next_page).click(next_page).perform()
+    # populate the lists
+    for i, row in enumerate(driver.find_elements(By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/div/div[2]/table/tbody/tr/td")):
+        rest = i % 6
+        if (rest == 0):
+                unknowns.append(row.text)
+        if (rest == 1):
+                permits.append(row.text)
+        if (rest == 2):
+            legal_names.append(row.text)
+        if (rest == 3):
+            also_knowns.append(row.text)
+        if (rest == 4):
+            status.append(row.text)
+        if (rest == 5):
+            locations.append(row.text)
 
+    # list into dataframe
+    df = pd.DataFrame(list(zip(*[permits, legal_names, also_knowns,status,locations])),columns=df_columns)
+    # df.rename(columns=df_columns)
+    # df = pd.DataFrame(list(zip(*[permits, legal_names, also_knowns,status,locations]))).add_prefix('Col')
+    return df
 
-# rows = driver.find_elements(By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/div/div[2]/table/tbody/tr[1]")
-# altrows = driver.find_elements(By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/div/div[2]/table/tbody/tr[1]/td[5]/div/div")
+for i in range(1,60):
+        time.sleep(5)
+        
+        df_new = scrape_page_to_df(outputcolumns)
+        print(df_new)
+        big_df = pd.concat([big_df,df_new])
+        # print(big_df)
+        # dataframe to csv
+        next_page = driver.find_element(By.CLASS_NAME, "k-i-arrow-60-right")
+        # next_page = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/div/div[2]/div/a[3]")
+        ActionChains(driver).move_to_element(next_page).click(next_page).perform()
+        print("Total Records = "+str(len(big_df)))
+        # big_df.to_csv(filename, index=False)
 
-# table = driver.find_element(By.CSS_SELECTOR,"tbody[role='rowgroup'")
-# row_count = len(driver.find_elements_by_xpath("//table[@role='rowgroup']/tbody/tr"))
-# column_count = len(driver.find_elements_by_xpath("//tabel[@role='rowgroup']/tbody/tr/td"))
-
-# for index in enumerate(rows):
-#     print(altrows[index[0]].text)
-# 
-
-# df = pd.read_html(table)[0]
-# df.columns = ['Date','Value']
-
-# soup = bs(table,'lxml')
-# print(soup)
-
-# print(firms)
-
-# names = element.find_elements(By.CSS_SELECTOR,"")
-#     element.find_element(By.TAG_NAME, "img").get_attribute("srcset") 
+print(len(big_df))
+big_df.to_csv(filename, index=False)
